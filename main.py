82298fb674a37a09c61e9f1117c5767406438d49
rdoroshko <rdoroshko@gmail.com>
2016-02-01 22:55:46 +0200

    request

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return 'Hello World! (%s)<br><a href="/add/2/3/">2+3</a>' % request.method
+
+
+@app.route('/add/<int:x>/<int:y>/')
+def add(x, y):
+    return '%d+%d=%d' % (x, y, x+y)
+
 
 if __name__ == '__main__':
-    # app.run()
-    app.run(host='0.0.0.0')
+    app.debug=True
+    app.run()
+    # app.run(host='0.0.0.0')
