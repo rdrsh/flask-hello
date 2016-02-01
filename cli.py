c4f69f5826d16f5a33948784bc67a76c172dd164
rdoroshko <rdoroshko@gmail.com>
2016-02-01 22:39:34 +0200

    cli.py

---
+++ cli.py
@@ -0,0 +1,12 @@
+from flask import url_for
+from main import app
+
+
+def t_url_for():
+    with app.test_request_context():
+        print(url_for('index'))
+        print(url_for('add', x=1, y=3))
+
+
+if __name__ == '__main__':
+    t_url_for()
