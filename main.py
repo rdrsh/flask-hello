b24af7d3a9f458d27daefe7aefaffc9f9d02797d
rdoroshko <rdoroshko@gmail.com>
2016-02-02 12:06:47 +0200

    templates filters content_processors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template
+import pprint
+from flask import Flask, request, render_template, Markup
 app = Flask(__name__)
 
 
@@ -10,6 +11,25 @@ def index():
 @app.route('/add/<int:x>/<int:y>/')
 def add(x, y):
     return '%d+%d=%d' % (x, y, x+y)
+
+
+@app.template_filter('pp')
+def filter_pp(x):
+    s = pprint.pformat(x, indent=4, width=80)
+    s = Markup.escape(s)
+    return Markup('<pre>%s</pre>' % s)
+
+
+@app.context_processor
+def inject_dict():
+    d = dict((x, x**2) for x in range(10))
+    return dict(d=d)
+
+@app.context_processor
+def inject_fn():
+    def f(n):
+        return 'xyx '*n
+    return dict(dub_str=f)
 
 
 if __name__ == '__main__':
